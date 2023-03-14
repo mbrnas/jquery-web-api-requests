d365c08b185dd420fc2582f7de78b3bbb900bb72
Matija Brnas <mb8828@rit.edu>
2023-03-14 23:12:35 +0100

    pokazuje data za undergraduate

--- js/Controller.js
+++ js/Controller.js
@@ -10,10 +10,21 @@ export class Controller {
         //1. send the request - the jqXHR object is returned, 
         //that will hold the response on success (available within the done() callback)
         let response = this.model.getData('/news');
+        let aboutResponse = this.model.getData('/about');
+
+        let undergraduateResponse = this.model.getData('/degrees/undergraduate');
 
         //2. specify what will be executed when the request is successful (done() callback)
         response.done((data) => {
             this.view.renderNewsSection(data);
         });
+
+        aboutResponse.done((data) => {
+            this.view.renderAboutSection(data);
+        });
+
+        undergraduateResponse.done((data) => {
+            this.view.renderUndergraduateSection(data);
+        })
     }
 }
--- js/View.js
+++ js/View.js
@@ -1,12 +1,13 @@
 export class View {
     constructor() {
       this.$newsSection = $("#section-news");
+      this.$aboutSection = $("#section-about");
+
+      this.$undergraduateSection = $("#section-undergraduate");
     }
   
     renderSpinner() {
       this.$newsSection.html('<img src="media/gears.gif" id="spinner"/>');
-      //select all sections which id starts with 'section-'
-      //$('[id^=section-]').html('<img src="media/gears.gif" id="spinner"/>');
     }
   
     renderNewsSection(data) {
@@ -27,5 +28,31 @@ export class View {
   
       this.$newsSection.append($newsDiv);
     }
+
+    renderAboutSection(data){
+        this.$aboutSection.html("");
+        this.$aboutSection.append(`<h1>${data.title}</h1>`);
+        this.$aboutSection.append(`<p>${data.description}</p>`);
+        this.$aboutSection.append(`<h5>${data.quoteAuthor}</h5>`);
+        this.$aboutSection.append(`<h5>${data.quote}</h5>`);
+    }
+
+
+    renderUndergraduateSection(data){
+        console.log(data.undergraduate);
+        this.$undergraduateSection.html("");
+        let $undergraduateDiv = $('<div></div>');
+
+        $.each(data.undergraduate, (index, item) => {
+            let $undergraduateItem = $(
+                `<h1>${item.degreeName}: ${item.title}</h1><div><p>${item.description}</p><ul><li>${item.concentrations}</li></ul></div>`
+            );
+
+           $undergraduateDiv.append($undergraduateItem);
+        });
+
+        this.$undergraduateSection.append($undergraduateDiv);
+    }
+
   }
   
